de77b2e2ba09bd0625a0323c71040125524a454d
Mono <byniqing@163.com>
2019-10-19 15:47:57 +0800

    add

---
+++ SD.Api/Controllers/UserServiceController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DnsClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SD.Api.Dtos;
+
+namespace SD.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserServiceController : ControllerBase
+    {
+        private HttpClient _httpClient;
+        //private readonly string _userServiceUrl = "http://localhost:5000";
+        private readonly string _userServiceUrl;
+        private readonly IDnsQuery _dns;
+        private readonly IOptions<ServiceDiscoveryOptions> _options;
+        public UserServiceController(HttpClient httpClient, IDnsQuery dns, IOptions<ServiceDiscoveryOptions> options)
+        {
+            _httpClient = httpClient;
+            _dns = dns ?? throw new ArgumentNullException(nameof(dns));
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+
+            var result = _dns.ResolveService("service.consul", _options.Value.ServiceName);
+            /*
+             如果服务注册用的是localhost,那么AddressList为空，则取HostName
+             必须是ip地址，比如127.0.0.1
+             */
+            var addressList = result.First().AddressList;
+            //var address = result.First().AddressList.FirstOrDefault();
+
+            var address = addressList.Any() ?
+                addressList.FirstOrDefault().ToString() :
+                result.First().HostName;
+            var port = result.First().Port;
+
+            _userServiceUrl = $"http://{address}:{port}";
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var service = await _dns.ResolveServiceAsync("service.consul", _options.Value.ServiceName);
+            var address = service.First().AddressList.FirstOrDefault();
+            var port = service.First().Port;
+
+            var response = await _httpClient.GetAsync(_userServiceUrl + "/api/users");
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ SD.Api/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using DnsClient;
+using SD.Api.Dtos;
+using System.Net.Http;
+
+namespace SD.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<ServiceDiscoveryOptions>(Configuration.GetSection("ServiceDiscovery"));
+            services.AddSingleton<IDnsQuery>(p =>
+            {
+                var serviceConfiguration = p.GetRequiredService<IOptions<ServiceDiscoveryOptions>>().Value;
+                var dnsEndpoint = serviceConfiguration.Consul.DnsEndpoint;
+
+                //推荐
+                return new LookupClient(serviceConfiguration.Consul.DnsEndpoint.ToIPEndPoint());
+                //或者
+                //return new LookupClient(IPAddress.Parse(dnsEndpoint.Address), dnsEndpoint.Port);
+                //return new LookupClient(IPAddress.Parse("127.0.0.1"), 8600);
+
+            });
+
+            //services.AddSingleton<IConsulClient>(p => new ConsulClient(cfg =>
+            //{
+            //    var serviceConfiguration = p.GetRequiredService<IOptions<ServiceDiscoveryOptions>>().Value;
+
+            //    if (!string.IsNullOrEmpty(serviceConfiguration.Consul.HttpEndpoint))
+            //    {
+            //        // if not configured, the client will use the default value "127.0.0.1:8500"
+            //        cfg.Address = new Uri(serviceConfiguration.Consul.HttpEndpoint);
+            //    }
+            //}));
+
+            services.AddSingleton<HttpClient>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
